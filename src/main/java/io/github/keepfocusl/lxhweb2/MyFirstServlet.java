8c1c2a22424e88eee0e7cfdaa21065fa83a079e8
KeepFocusL <1281438594@qq.com>
2024-09-02 21:02:40 +0800

    新建第一个 Servlet

---
+++ src/main/java/io/github/keepfocusl/lxhweb2/MyFirstServlet.java
@@ -0,0 +1,34 @@
+package io.github.keepfocusl.lxhweb2;
+
+import jakarta.servlet.*;
+import jakarta.servlet.annotation.WebServlet;
+
+import java.io.IOException;
+@WebServlet("/my-first-servlet")
+public class MyFirstServlet implements Servlet {
+
+    @Override
+    public void init(ServletConfig servletConfig) throws ServletException {
+
+    }
+
+    @Override
+    public ServletConfig getServletConfig() {
+        return null;
+    }
+
+    @Override
+    public void service(ServletRequest servletRequest, ServletResponse servletResponse) throws ServletException, IOException {
+        System.out.println("MyFirstServlet.service");
+    }
+
+    @Override
+    public String getServletInfo() {
+        return "";
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+}
