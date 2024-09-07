2c19174db432ab179169ac119735907f476babc3
KeepFocusL <1281438594@qq.com>
2024-09-07 18:01:04 +0800

    HTTP 协议中典型的方法之一：GET ...
    doGet: 增加 login.html 页面，同时增加 login Servlet，在 Servlet 中获取用户输入的内容...
    
    method="get" 的方式，会把数据显示在浏览器的地址栏中，不安全

--- src/main/java/io/github/keepfocusl/lxhweb2/day240904/MyHttpServlet.java
+++ src/main/java/io/github/keepfocusl/lxhweb2/day240904/MyHttpServlet.java
@@ -12,7 +12,9 @@ import java.io.IOException;
 public class MyHttpServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        System.out.println("MyHttpServlet.doGet");
+        String username = req.getParameter("username");
+        String password = req.getParameter("password");
+        System.out.println("MyHttpServlet.doGet: username=  " + username + ", password=" + password);
     }
 
     @Override
