b2820a5ab5cfeb71c500bee91ac2419fa32e491e
KeepFocusL <1281438594@qq.com>
2024-09-07 17:53:17 +0800

    给 MyHttpServlet 指定 URL

--- src/main/java/io/github/keepfocusl/lxhweb2/day240904/MyHttpServlet.java
+++ src/main/java/io/github/keepfocusl/lxhweb2/day240904/MyHttpServlet.java
@@ -1,12 +1,14 @@
 package io.github.keepfocusl.lxhweb2.day240904;
 
 import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
 
+@WebServlet("/login")
 public class MyHttpServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
