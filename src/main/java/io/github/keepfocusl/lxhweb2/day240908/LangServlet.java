cb40cbd0f10535a3bd495844a1775739d0a68f0d
KeepFocusL <1281438594@qq.com>
2024-09-08 13:46:14 +0800

    课堂作业：LangServlet 通过 response 响应内容给 Client

--- src/main/java/io/github/keepfocusl/lxhweb2/day240908/LangServlet.java
+++ src/main/java/io/github/keepfocusl/lxhweb2/day240908/LangServlet.java
@@ -12,7 +12,7 @@ import java.io.IOException;
 public class LangServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        System.out.println("LangServlet.doGet");
+        response.getWriter().write("LangServlet");
     }
 
     @Override
