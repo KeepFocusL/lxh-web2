6b7f003c8ade8ce96ad700adda55cf8a9e8d05b0
KeepFocusL <1281438594@qq.com>
2024-09-09 16:43:20 +0800

    一个 Servlet 可以配置多个 URL...
    @WebServlet(urlPatterns={"/aaa", "/bbb"})

--- src/main/java/io/github/keepfocusl/lxhweb2/day240908/LangServlet.java
+++ src/main/java/io/github/keepfocusl/lxhweb2/day240908/LangServlet.java
@@ -8,7 +8,7 @@ import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
 
-@WebServlet("/lang")
+@WebServlet(urlPatterns = {"/lang","/lan"})
 public class LangServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
