94a47345c2fdac7573a4bca4fa9a0940f8cd80fd
KeepFocusL <1281438594@qq.com>
2024-09-20 20:38:45 +0800

    ResponseServlet2 - 复习 response.getWriter().write(...)

---
+++ src/main/java/io/github/keepfocusl/lxhweb2/day240918/ResponseServlet2.java
@@ -0,0 +1,25 @@
+package io.github.keepfocusl.lxhweb2.day240918;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet("/ResponseServlet2")
+public class ResponseServlet2 extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        PrintWriter writer = response.getWriter();
+        response.setHeader("content-type","text/html");
+        writer.write("<h1>text...</h1>");
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        this.doGet(request, response);
+    }
+}
