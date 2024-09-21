536096f89571447ac1a936656beed6d8b517ed4e
KeepFocusL <1281438594@qq.com>
2024-09-21 12:47:54 +0800

    HW: 简历页面, 支持简历下载

---
+++ src/main/java/io/github/keepfocusl/lxhweb2/day240921/ResumeServlet.java
@@ -0,0 +1,30 @@
+package io.github.keepfocusl.lxhweb2.day240921;
+
+import jakarta.servlet.ServletOutputStream;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.apache.commons.io.IOUtils;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.net.URLEncoder;
+
+
+@WebServlet("/day240921/resume-download")
+public class ResumeServlet extends HttpServlet {
+    @Override
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        String path = this.getServletContext().getRealPath("/day240921/resume-demo.pdf");
+        FileInputStream fis = new FileInputStream(path);
+
+        ServletOutputStream sos = response.getOutputStream();
+
+        response.setHeader("Content-Disposition", "attachment;filename=" + URLEncoder.encode("我的简历-xxx.pdf", "UTF-8"));
+
+        IOUtils.copy(fis, sos);
+
+        fis.close();
+    }
+}
