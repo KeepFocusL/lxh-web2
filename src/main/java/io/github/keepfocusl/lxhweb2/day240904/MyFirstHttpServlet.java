93c38fbef267f7b8c91afb93ba3ac7e0e4e5f428
KeepFocusL <1281438594@qq.com>
2024-09-25 15:09:48 +0800

    修改 WebServlet 名

--- src/main/java/io/github/keepfocusl/lxhweb2/day240904/MyFirstHttpServlet.java
+++ src/main/java/io/github/keepfocusl/lxhweb2/day240904/MyFirstHttpServlet.java
@@ -8,7 +8,7 @@ import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
 
-@WebServlet("/login")
+@WebServlet("/login1")
 public class MyFirstHttpServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
